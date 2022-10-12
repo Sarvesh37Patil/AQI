4e3dff29141affde3759ee1b2cb57dd31e49395a
Sarvesh Patil <sarveshcpatil@gmail.com>
2022-10-12 20:52:39 +0530

    formatted visuals for the landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        good: "linear-gradient(to right, #56ab2f, #a8e063)",
+        fair: "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)",
+        moderate: "linear-gradient(to left, #d4fc79 0%, #96e6a1 100%)",
+        poor: "linear-gradient(to left, #f46b45, #eea849)",
+        bad: "linear-gradient(to right, #cb2d3e, #ef473a)",
+      },
+    },
+  },
+  plugins: [],
+};
