cbabfdbf1fcaaba76dd4efa380217f86094bd8ee
Sarvesh Patil <sarveshcpatil@gmail.com>
2022-10-13 00:24:38 +0530

    Improved responsiveness on layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       backgroundImage: {
         good: "linear-gradient(to right, #56ab2f, #a8e063)",
         fair: "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)",
-        moderate: "linear-gradient(to left, #d4fc79 0%, #96e6a1 100%)",
+        moderate: "linear-gradient(to left, #F8EF42 0%, #FCE043 100%)",
         poor: "linear-gradient(to left, #f46b45, #eea849)",
         bad: "linear-gradient(to right, #cb2d3e, #ef473a)",
       },
