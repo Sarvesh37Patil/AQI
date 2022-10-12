769ca3e9f771410fd0cf6f3d4c2ed39ff6b7324a
Sarvesh Patil <sarveshcpatil@gmail.com>
2022-10-13 01:38:34 +0530

    vercel integration removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   vite: {
     ssr: {
-      external: ["svgo"]
-    }
+      external: ["svgo"],
+    },
   },
   output: "server",
-  adapter: vercel()
 });
